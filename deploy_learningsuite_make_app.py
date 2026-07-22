40b5290b5a3753b9c2a555d8d49c131aef459db1
belal-rh <belal.karimzai@raykhahne.de>
2026-07-22 16:54:16 +0200

    adding loading env

--- deploy_learningsuite_make_app.py
+++ deploy_learningsuite_make_app.py
@@ -4,5 +4,10 @@
 from learningsuite_make_deployer.cli import main
 
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
 if __name__ == "__main__":
     raise SystemExit(main())
